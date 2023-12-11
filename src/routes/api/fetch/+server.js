357ecc2dbc131764e43d0892f722e534e019df97
mattjoke <matejhakos@gmail.com>
2023-12-11 17:28:45 +0100

    feat: regenerated pnpm file

--- src/routes/api/fetch/+server.js
+++ src/routes/api/fetch/+server.js
@@ -1,17 +1,17 @@
-import {readFile} from "fs";
 import {dataLoadingState, dataStore} from "$lib/stores/stores.js";
 import {LoadingState} from "$lib/objects/loadingState";
 import {json} from "@sveltejs/kit";
+import data from "$lib/data/perun.json"
 
 export async function GET(event) {
     console.log("Fetching data from file");
-    await readFile('static/data/perun.json', 'utf8', (err, data) => {
-        if (err) {
-            dataLoadingState.set(LoadingState.ERROR);
-            return json({process: "ERROR", error: err});
-        }
-        dataLoadingState.set(LoadingState.LOADED);
-        dataStore.set(JSON.parse(data));
-    });
+    dataLoadingState.set(LoadingState.LOADED);
+    dataStore.set(data);
+    // await readFile('static/data/perun.json', 'utf8', (err, data) => {
+    //     if (err) {
+    //         dataLoadingState.set(LoadingState.ERROR);
+    //         return json({process: "ERROR", error: err});
+    //     }
+    // });
     return json({process: "OK"});
 }
